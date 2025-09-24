0c2ca036295851cbb4b07d72d632ee9982cf9c05
GIRAM PRAVEEN <162723646+Praveen0329@users.noreply.github.com>
2025-09-24 18:51:34 +0530

    Update spotlight.js

--- spotlight.js
+++ spotlight.js
@@ -1,11 +1,11 @@
 const products = [
-  { id: 1, name: "Men's T-Shirt", price: "₹799", image: "images/product1.jpg" },
-  { id: 2, name: "Women's Dress", price: "₹1299", image: "images/product2.jpg" },
-  { id: 3, name: "Men's Jeans", price: "₹1499", image: "images/product3.jpg" },
-  { id: 4, name: "Women's Top", price: "₹999", image: "images/product4.jpg" },
-  { id: 5, name: "Men's Jeans", price: "₹1499", image: "images/product5.jpg" },
-  { id: 6, name: "Women's Top", price: "₹999", image: "images/product6.jpg" },
-  { id: 7, name: "Men's Shirt", price: "₹999", image: "images/product7.jpg" }
+  { id: 1, name: "Men's T-Shirt", price: "₹799", image: "product1.jpg" },
+  { id: 2, name: "Women's Dress", price: "₹1299", image: "product2.jpg" },
+  { id: 3, name: "Men's Jeans", price: "₹1499", image: "product3.jpg" },
+  { id: 4, name: "Women's Top", price: "₹999", image: "product4.jpg" },
+  { id: 5, name: "Men's Jeans", price: "₹1499", image: "product5.jpg" },
+  { id: 6, name: "Women's Top", price: "₹999", image: "product6.jpg" },
+  { id: 7, name: "Men's Shirt", price: "₹999", image: "product7.jpg" }
 ];
 var grid = document.getElementById("productGrid");
 
@@ -36,3 +36,4 @@ for (var i = 0; i < products.length; i++) {
 
   grid.appendChild(card);
 }
+
